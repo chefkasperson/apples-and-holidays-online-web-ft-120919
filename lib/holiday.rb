aefc9990ac26d929cdee19b9a41f13487ec6a26a
Erik Kasperson <chefkasperson@gmail.com>
2019-12-12 09:30:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,5 +74,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq_day = []
+  holiday_hash.each {|season, holiday|
+    holiday.each {|holiday, supply|
+      if supply.incude?("BBQ")
+        bbq_day << holiday
+      end
+    }
+  }
 end
